b21bf1389af378e711655cc5f9e216b1d9b75ee9
zhouhuahui <2957286012@qq.com>
2023-02-18 16:23:00 +0800

    test

--- src/rdma_communication.cpp
+++ src/rdma_communication.cpp
@@ -893,7 +893,8 @@ void CommonRdmaClient::sendThreadFun(uint32_t node_idx) {
     for (int i = 0; i < rc; ++i) {
       struct ibv_wc &wc = wcs[i];
       if (wc.status != IBV_WC_SUCCESS) {
-        LOG_DEBUG("CommonRdmaClient send thread of %u, get a failed work completion", node_idx);
+        LOG_DEBUG("CommonRdmaClient send thread of %u, get a failed work completion"
+                ", wc.status is %d", node_idx, wc.status);
         return;
       }
       if (wc.opcode == IBV_WC_RECV_RDMA_WITH_IMM) {
