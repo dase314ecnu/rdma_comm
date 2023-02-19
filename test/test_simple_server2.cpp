67ed6b63713b7e6511f8e311ca06bb0a18dd79af
zhouhuahui <2957286012@qq.com>
2023-02-19 14:25:21 +0800

    test

--- test/test_simple_server2.cpp
+++ test/test_simple_server2.cpp
@@ -167,18 +167,18 @@ int main() {
     if (IS_SERVER) {
         // 由于是测试，所以我写的工作线程很简单，所有工作线程都从一个队列中取数据
         // 因此如果将工作线程的数量设置的太大，则会大大增加竞争，性能非常差
-        test.TestSimpleServer2(IsServer{}, 3, 1000, 5, 64, 500);
+        test.TestSimpleServer2(IsServer{}, 3, 1000, 1, 64, 500);
     } else {
         // node_num: 5
         // slot_size: 64
         // slot_num: 50
         // num_test_thread: 1000  有num_test_thread个线程同时来发送请求
         // reqs_per_test_thread: 1000 每个线程发送reqs_per_test_thread个请求
-        test.TestSimpleServer2(IsClient{}, 5, 64, 500, 1000, 1000);
+        test.TestSimpleServer2(IsClient{}, 1, 64, 500, 1000, 1000);
     }
 }
 
 /**  
- * @todo: 为什么和qp_num和slot_num设置的过大，比如50和500，会造成wc.status出现错误码8和10
+ * @todo: 为什么slot_num设置的过大，比如500，会造成wc.status出现错误码8和10
  * @todo: 实现批量发送，批量响应，批量将请求加入到请求队列
  */
