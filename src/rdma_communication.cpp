b555275522e2bb487ff6a7ae4ff5121486461077
zhouhuahui <2957286012@qq.com>
2023-02-22 20:58:17 +0800

    test

--- src/rdma_communication.cpp
+++ src/rdma_communication.cpp
@@ -396,7 +396,7 @@ int RdmaQueuePair::ReadyToUseQP() {
     return -1;
   }
   // zhouhuahui test
-  for (int i = 0; i < this->local_slot_num * 2; ++i) {
+  for (int i = 0; i < this->local_slot_num; ++i) {
     if (this->PostReceive() != 0) {
       LOG_DEBUG("RdmaQueuePair failed to post %d receives in qp", i);
       return -1;
@@ -1087,12 +1087,11 @@ void SharedRdmaClient::sendThreadFun(uint32_t node_idx) {
       (void) pthread_spin_lock(send->spinlock);
       uint64_t slot_idx = send->notsent_front;
       while (slot_idx != send->notsent_rear) {
-        // zhouhuahui test
-        // if (qp->PostReceive() != 0) {
-        //   LOG_DEBUG("SharedRdmaClient sendThreadFun, send thread of %u, failed to post receive "
-        //           "before posting a send request", node_idx);
-        //   return;
-        // }
+        if (qp->PostReceive() != 0) {
+          LOG_DEBUG("SharedRdmaClient sendThreadFun, send thread of %u, failed to post receive "
+                  "before posting a send request", node_idx);
+          return;
+        }
         rc = this->rdma_queue_pairs[node_idx]->PostSend(slot_idx, slot_idx);
         if (rc != 0) {
           (void) pthread_spin_unlock(send->spinlock);
