2b875142652485e1910bb5d6756ec9df38421eab
zhouhuahui <2957286012@qq.com>
2023-02-22 22:29:49 +0800

    test

--- src/rdma_communication.cpp
+++ src/rdma_communication.cpp
@@ -396,7 +396,7 @@ int RdmaQueuePair::ReadyToUseQP() {
     return -1;
   }
   // zhouhuahui test
-  for (int i = 0; i < this->local_slot_num + 1; ++i) {
+  for (int i = 0; i < this->local_slot_num + 10; ++i) {
     if (this->PostReceive() != 0) {
       LOG_DEBUG("RdmaQueuePair failed to post %d receives in qp", i);
       return -1;
