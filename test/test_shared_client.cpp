69714d935d5e688ef600d37cc5349d219e4749e7
zhouhuahui <2957286012@qq.com>
2023-02-24 01:27:48 +0800

    test

--- test/test_shared_client.cpp
+++ test/test_shared_client.cpp
@@ -81,6 +81,8 @@ void TestSharedClientClass::runClient() {
             is_father = false;
             sleep(3);  // 等待SharedRdmaClient在_shared_memory上初始化好
             rdma_client = (SharedRdmaClient *)_shared_memory;
+            LOG_DEBUG("zhouhuahui test: listenfd[0][0] = %d, listendfd[0][1] = %d", listen_fd[0][0],
+                    listen_fd[0][1]); // zhouhuahui test
             func(i);
             return;
         }
