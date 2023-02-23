550bce692671a48c2820efa55cf41b43575ef158
zhouhuahui <2957286012@qq.com>
2023-02-23 23:15:59 +0800

    test

--- src/rdma_communication.cpp
+++ src/rdma_communication.cpp
@@ -1394,10 +1394,12 @@ int SharedRdmaClient::PostRequest(void *send_content, uint64_t size) {
   //   usleep(100);
   // }
 
+
+
   // zhouhuahui test
-  pthread_spin_lock(this->sends[0].spinlock);
-  sleep(10);
-  pthread_spin_unlock(this->sends[0].spinlock);
+  // pthread_spin_lock(this->sends[0].spinlock);
+  // sleep(10);
+  // pthread_spin_unlock(this->sends[0].spinlock);
 }
 
 uint64_t SharedRdmaClient::GetSharedObjSize(uint64_t _slot_size, uint64_t _slot_num, 
