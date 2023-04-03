81f41168faecad5028affda6d58efa0ba36e911f
zhouhuahui <2957286012@qq.com>
2023-04-03 19:00:22 +0800

    update

--- src/pgrac_rdma_communication.cpp
+++ src/pgrac_rdma_communication.cpp
@@ -121,8 +121,8 @@ int RdmaQueuePair::createQueuePair() {
   attr.sq_sig_all = 0;
   attr.send_cq = this->cq;
   attr.recv_cq = this->cq;
-  attr.cap.max_send_wr = this->local_slot_num;
-  attr.cap.max_recv_wr = this->local_slot_num * 2;
+  attr.cap.max_send_wr = this->local_slot_num * GROUP_POST_SEND_MAX_MSG_NUM;
+  attr.cap.max_recv_wr = this->local_slot_num * GROUP_POST_SEND_MAX_MSG_NUM;
   attr.cap.max_send_sge = 2;
   attr.cap.max_recv_sge = 1;
   attr.cap.max_inline_data = 0;
