3007b6fddb132f542b71e1a52e5ece3399226a28
zhouhuahui <2957286012@qq.com>
2023-04-03 13:25:18 +0800

    update

--- include/pgrac_configuration.h
+++ include/pgrac_configuration.h
@@ -15,9 +15,9 @@
 #define IS_SERVER (1)   // 是否是RdmaServer
 #define SERVER_IP ("49.52.27.135")  //RdmaServer的地址
 // #define TEST_SHARED_MEMORY
-#define TEST_SIMPLE_SERVER
+// #define TEST_SIMPLE_SERVER
 // #define TEST_SIMPLE_SERVER2
-// #define TEST_SHARED_CLIENT
+#define TEST_SHARED_CLIENT
 
 
 /** 
