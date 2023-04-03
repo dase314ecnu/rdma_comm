4a78cb46a7127f3e827b1883c4132db5e2ad946e
zhouhuahui <2957286012@qq.com>
2023-04-03 23:31:06 +0800

    test

--- test/test_shared_client.cpp
+++ test/test_shared_client.cpp
@@ -61,7 +61,10 @@ void TestSharedClientClass::runClient() {
             // zhouhuahui test
             LOG_DEBUG("test_process of %u will send %dth(from 0) msg", test_process_idx, j);
             void *response = nullptr;
-            rdma_client->PostRequest((void *)send_buf, length, &response);
+            // rdma_client->PostRequest((void *)send_buf, length, &response);
+            int rc = 0;
+            auto wait = rdma_client->AsyncPostRequest((void *)send_buf, length, &rc);
+            wait(&response);
             LOG_DEBUG("test_process of %u has sent %dth(from 0) msg, get response length: %d", 
                     test_process_idx, j, MessageUtil::parseLength(response));
             free(response);
