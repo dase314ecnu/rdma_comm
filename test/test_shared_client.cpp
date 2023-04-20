6bd8f006345b8fdf5a62f34f242190bcc75b74d3
zhouhuahui <2957286012@qq.com>
2023-04-20 17:12:43 +0800

    test

--- test/test_shared_client.cpp
+++ test/test_shared_client.cpp
@@ -157,14 +157,14 @@ int main() {
     if (IS_SERVER) {
         // 由于是测试，所以我写的工作线程很简单，所有工作线程都从一个队列中取数据
         // 因此如果将工作线程的数量设置的太大，则会大大增加竞争，性能非常差
-        test.TestSimpleServer2(IsServer{}, 3, 50, 5, 1024, 500); 
+        test.TestSimpleServer2(IsServer{}, 3, 50, 6, 1024, 500); 
     } else {
         // node_num: 5
         // slot_size: 64
         // slot_num: 500
         // num_test_thread: 100  有num_test_thread个线程同时来发送请求
         // reqs_per_test_thread: 100 每个线程发送reqs_per_test_thread个请求
-        test.TestSimpleServer2(IsClient{}, 5, 1024, 500, 100, 1000);
+        test.TestSimpleServer2(IsClient{}, 6, 1024, 500, 30, 1000);
     }
 }
 
