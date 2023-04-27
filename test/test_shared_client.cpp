f81759becc83cb2476a4d32ac5829fba437d9b7f
zhouhuahui <2957286012@qq.com>
2023-04-27 15:42:42 +0800

    test

--- test/test_shared_client.cpp
+++ test/test_shared_client.cpp
@@ -180,7 +180,7 @@ int main() {
         // slot_num: 500
         // num_test_thread: 100  有num_test_thread个线程同时来发送请求
         // reqs_per_test_thread: 100 每个线程发送reqs_per_test_thread个请求
-        test.TestSimpleServer2(IsClient{}, 6, 1024, 500, 2, 100000);
+        test.TestSimpleServer2(IsClient{}, 6, 1024, 500, 10, 100000);
     }
 }
 
