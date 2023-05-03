25c740dacbc6d4605324075f65f57484ab8d2d2f
zhouhuahui <2957286012@qq.com>
2023-05-03 20:09:55 +0800

    test

--- include/pgrac_configuration.h
+++ include/pgrac_configuration.h
@@ -20,7 +20,7 @@
 #define IMM_DATA_SLOT_IDX_MASK (0xFFFFFF00)
 #define IMM_DATA_MSG_NUM_MASK (0x000000FF)
 #define IMM_DATA_SHIFT (8)
-#define GROUP_POST_SEND_MAX_MSG_NUM (5)
+#define GROUP_POST_SEND_MAX_MSG_NUM (15)
 #define GET_SLOT_IDX_FROM_IMM_DATA(imm_data) \
     ((imm_data & IMM_DATA_SLOT_IDX_MASK) >> IMM_DATA_SHIFT)
 #define SET_SLOT_IDX_TO_IMM_DATA(imm_data, slot_idx) \
